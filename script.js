bd0f8203678c6b50966ec41dfaf19b2cb06b4cec
Steev John <73013870+steev-john@users.noreply.github.com>
2025-02-12 22:11:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const yesBtn = document.getElementById("yesBtn");
     const noBtn = document.getElementById("noBtn");
     const container = document.querySelector('.container');
-    const messages = ['Try harder! 😜', 'Nope! 🙅♀️', 'Not today! 😅', 'Catch me! 🏃♀️'];
+    const messages = ['Try harder! 😜', 'Podi 😤, 'Nope! 🙅♀️', 'Not today! 😅', 'Catch me! 🏃♀️'];
     const MIN_DISTANCE = 80;
     let isMoving = false;
 
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         setTimeout(() => {
             isMoving = false;
-        }, 400);
+        }, 40);
     }
 
     noBtn.addEventListener('touchstart', (e) => {
